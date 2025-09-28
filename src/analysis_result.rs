0775c8fc254b09edf2d2e116f03be45a912f806f
Matthieu Casanova <chocolat.mou@gmail.com>
2025-09-28 22:57:46 +0200

    make clippy happier

--- src/analysis_result.rs
+++ src/analysis_result.rs
@@ -18,14 +18,14 @@ impl AnalysisResult {
 
     pub(crate) fn selected_index(&mut self, index: usize) {
         while index < self.data_stack.len() - 1 {
-            if let Some(popped_data) = self.data_stack.pop() {
-                if let Some(parent_data) = self.data_stack.last_mut() {
-                    if let Kind::Dir(children) = &mut parent_data.kind {
-                        info!("Pushing {} into {}", popped_data.name, parent_data.name);
-                        children.push(popped_data);
-                    } else {
-                        log::error!("Invalid kind ({parent_data:?})");
-                    }
+            if let Some(popped_data) = self.data_stack.pop()
+                && let Some(parent_data) = self.data_stack.last_mut()
+            {
+                if let Kind::Dir(children) = &mut parent_data.kind {
+                    info!("Pushing {} into {}", popped_data.name, parent_data.name);
+                    children.push(popped_data);
+                } else {
+                    log::error!("Invalid kind ({parent_data:?})");
                 }
             }
         }
